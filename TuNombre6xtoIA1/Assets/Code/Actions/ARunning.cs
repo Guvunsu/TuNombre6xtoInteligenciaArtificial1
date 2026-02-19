288e60fe214ad622305c18385fb1f8dd0941f89c
Guvunsu <gavryk_fray_rojas@hotmail.com>
2026-02-19 13:51:02 -0600

    clase 19 02 2026

---
+++ TuNombre6xtoIA1/Assets/Code/Actions/ARunning.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class ARunning : GOAPAction
+{
+    private Animator animator;
+    private UICanvasEmotions script_UICanvasEmotions;
+
+    public int foodConsumed = 1;
+    public float eatDuration = 0;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        script_UICanvasEmotions = GetComponent<UICanvasEmotions>();
+        duration = eatDuration;
+        AddNumericPrecondition("Food", CompareOp.GreaterOrEqual, foodConsumed);
+        AddEffect("IsFull", true);
+        AddNumericEffect("Food", EffectOp.Subtract, foodConsumed);
+
+        AddPrecondition("AgentIsClose", true);
+
+        AddEffect("Sit&Talking", false);
+        AddEffect("IsFull", false);
+        AddEffect("HasFood", false);
+        AddEffect("IsTired", false);
+        AddEffect("Running", true);
+
+        cost = 1f;
+    }
+    void ResetAnimations()
+    {
+        animator.SetBool("IsWalking", false);
+        animator.SetBool("IsSleeping", false);
+        animator.SetBool("IsWorking", false);
+        animator.SetBool("IsGetFood", false);
+        animator.SetBool("IsEating", false);
+        animator.SetBool("IsRunning", true);
+    }
+    protected override void OnStart(WorldState state)
+    {
+        Debug.Log("AEat: start");
+        ResetAnimations();
+        if (animator != null)
+        {
+            animator.SetBool("IsEating", true);
+            //script_UICanvasEmotions.SetMood(EmotionReferenceInAgent.HAPPYNESS);
+        }
+    }
+    protected override void OnTick(WorldState state, float t01, float elapsed)
+    {
+        // opcional: algo durante, sonido, UI, etc.
+        // Debug.Log($"AEat progress {t01:0.00}");
+    }
+    protected override void OnComplete(WorldState state)
+    {
+        Debug.Log("AEat: complete");
+        if (animator != null)
+        {
+            animator.SetBool("IsEating", false);
+            //script_UICanvasEmotions.SetMood(EmotionReferenceInAgent.HAPPYNESS);
+        }
+        state["IsFull"] = true;
+        foreach (var e in NumericEffects)
+        {
+            e.Apply(state);
+        }
+    }
+}
